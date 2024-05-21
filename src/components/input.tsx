7bfe2a0fcf98522060abb1f2899a3260bb8f7f74
Fellipe Nogueira Milomem <fellipemilomem@hotmail.com>
2024-05-21 19:01:38 -0300

    signIn page

---
+++ src/components/input.tsx
@@ -0,0 +1,10 @@
+import { FormControl, Input as GluestackInput} from '@gluestack-ui/themed'
+
+export function Input({}) {
+    return (
+        <FormControl mb="$4">
+            <GluestackInput>
+            </GluestackInput>
+        </FormControl>
+    )
+}
--- src/screen/signIn.tsx
+++ src/screen/signIn.tsx
@@ -1,9 +1,21 @@
-import { View, Text } from "react-native";
+import { Center, Heading, ScrollView, Text, VStack } from "@gluestack-ui/themed";
+
+import LogoSvg from '@assets/logo.svg';
+
 
 export function SignIn() {
   return (
-    <View>
-        <Text>Sign In</Text>
-    </View>
+    <ScrollView contentContainerStyle={{ flexGrow: 1}} showsHorizontalScrollIndicator={false}>
+        <VStack flex={1} px={3} py={5}>
+            <Center mt={109} mb={76}>
+                <LogoSvg/>
+                <Heading mt={30} fontSize={"$4xl"}>marketspace</Heading>
+                <Text fontSize={"$sm"}>Seu espaço de compra e venda</Text>
+            </Center>
+            <Center>
+              
+            </Center>
+        </VStack>
+    </ScrollView>
   );
 }
